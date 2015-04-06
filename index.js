03d949a89a5988573b9d3a0df2c087880de908dd
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-06 18:44:51 -0400

    added old nodejs example

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var port = 3700;
+ 
+//app.get("/", function(req, res){
+//    res.send("It works!");
+//});
 
-app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var io = require('socket.io').listen(app.listen(port));
+
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+console.log("Listening on port " + port);
