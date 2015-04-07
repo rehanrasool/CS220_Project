2b27a5337a34339fd11c7d54517cc1fe710239af
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-07 19:28:15 -0400

    added php script

--- public/js/home.js
+++ public/js/home.js
@@ -9,7 +9,7 @@ $(function(){
   	({
       type: "POST",
       //the url where you want to sent the userName and password to
-      url: "../server/find_dehari.php",
+      url: "../server/scripts.php",
       //json object to sent to the authentication url
       data : {
       get_user_pads: 1,
