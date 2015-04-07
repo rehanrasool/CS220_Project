60dd7f9e417e33cecb9f175fd289f7b29eb8a620
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-07 18:07:19 -0400

    added home js file

---
+++ public/js/home.js
@@ -0,0 +1,42 @@
+// This file is executed in the browser, when people visit /chat/<random id>
+
+$(function(){
+
+	// getting the id of the room from the url
+	var id = Number(window.location.pathname.match(/\/chat\/(\d+)$/)[1]);
+
+	// connect to the socket
+	var socket = io();
+
+	// cache some jQuery objects
+	// var section = $(".section"),
+	// 	footer = $("footer"),
+	// 	onConnect = $(".connected"),
+	// 	inviteSomebody = $(".invite-textfield"),
+	// 	personInside = $(".personinside"),
+	// 	chatScreen = $(".chatscreen"),
+	// 	left = $(".left"),
+	// 	noMessages = $(".nomessages"),
+	// 	tooManyPeople = $(".toomanypeople");
+
+	// // some more jquery objects
+	// var chatNickname = $(".nickname-chat"),
+	// 	leftNickname = $(".nickname-left"),
+	// 	loginForm = $(".loginForm"),
+	// 	yourName = $("#yourName"),
+	// 	yourEmail = $("#yourEmail"),
+	// 	hisName = $("#hisName"),
+	// 	hisEmail = $("#hisEmail"),
+	// 	chatForm = $("#chatform"),
+	// 	textarea = $("#message"),
+	// 	messageTimeSent = $(".timesent"),
+	// 	chats = $(".chats");
+
+	// // these variables hold images
+	// var ownerImage = $("#ownerImage"),
+	// 	leftImage = $("#leftImage"),
+	// 	noMessagesImage = $("#noMessagesImage");
+
+
+
+});
