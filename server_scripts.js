6c324e69d0234a31160bbcb67e418a52e2286745
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-07 18:42:08 -0400

    json parse removed

--- server_scripts.js
+++ server_scripts.js
@@ -45,7 +45,7 @@ module.exports = function(app, io){
           else
            { 
                     // Generate unique id for the room
-            var id = JSON.parse(result.rows);
+            var id = result.rows['id'];
             //response.send(result.rows);
             response.send(id);
             // Redirect to the random room
