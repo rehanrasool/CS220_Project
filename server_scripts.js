1362f5c2c911ab45ee5f076b6ac0f19b9e49c5ca
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-07 14:24:58 -0400

    checking nodejs authentication script

---
+++ server_scripts.js
@@ -0,0 +1,42 @@
+var pg = require('pg');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+module.exports = function(app, io){
+
+    // parse application/json
+  app.use(bodyParser.json());                        
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.send(result.rows); }
+      });
+    });
+  })
+
+
+  app.post('/signup', function(request, response) {
+      var username = request.body.inputUsername;
+      var password = request.body.inputPassword;
+      console.log("post received: %s %s", username, password);
+
+      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT id FROM user_table WHERE username = ' + username + ' AND password = ' + password + ';' , function(err, result) {
+          done();
+          if (err)
+           { console.error(err); response.send("Error " + err); }
+          else
+           { response.send(result.rows); }
+        });
+      });
+  });
+
+};
+
