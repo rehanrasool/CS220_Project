0dbf19a525235e0f180e3da6abcd670f7bc63518
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-08 15:41:18 -0400

    append pad data to table

--- public/js/home.js
+++ public/js/home.js
@@ -13,7 +13,15 @@ $(function(){
       user_id : chimpad_user_id
     } }).done(function(raw_data) {
       
-      var data = JSON.parse(raw_data);
+      	var tbody = $('#user_pad_table tbody'),
+    	props = ["id", "title", "content", "last_modified_timestamp", "last_modified_user"];
+		$.each(raw_data, function(i, pad) {
+		  var tr = $('<tr>');
+		  $.each(props, function(i, prop) {
+		    $('<td>').html(pad[prop]).appendTo(tr);  
+		  });
+		  tbody.append(tr);
+		});
 
   	});
 
