d8447d1616acc3890b4552b9e2fab8d435c4a49b
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-09 18:32:16 -0400

    post meeting: modified create

--- server_scripts.js
+++ server_scripts.js
@@ -161,24 +161,3 @@ module.exports = function(app, io){
 
 
 };
-
-
-function findClientsSocket(io, roomId, namespace) {
-  var res = [],
-    ns = io.of(namespace ||"/");    // the default namespace is "/"
-
-  if (ns) {
-    for (var id in ns.connected) {
-      if(roomId) {
-        var index = ns.connected[id].rooms.indexOf(roomId) ;
-        if(index !== -1) {
-          res.push(ns.connected[id]);
-        }
-      }
-      else {
-        res.push(ns.connected[id]);
-      }
-    }
-  }
-  return res;
-}
