af61a86a59a30a811553b5e16ccefd8189cfd3b6
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-23 14:44:17 -0400

    testing create pad

--- server_scripts.js
+++ server_scripts.js
@@ -141,7 +141,7 @@ module.exports = function(app, io) {
     var date = new Date();
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      save_or_update_pad_query = 'INSERT INTO pad (title,last_modified_timestamp,last_modified_user) VALUES ("' + chimpad_pad_title + '","' + date.getDate() + '",' + chimpad_pad_user + ' RETURNING id;';
+      save_or_update_pad_query = 'INSERT INTO pad (title,last_modified_timestamp,last_modified_user) VALUES ("' + chimpad_pad_title + '","' + date.getDate() + '",' + chimpad_pad_user + ') RETURNING id;';
 
       client.query(save_or_update_pad_query , function(err, result) {
         done();
@@ -152,7 +152,7 @@ module.exports = function(app, io) {
             var chimpad_pad_id = result.rows;
             console.log("pad created with id " + chimpad_pad_id);
             //response.send(result.rows);
-            update_user_pads_query = 'INSERT INTO user_pad(user_id,pad_id,admin) VALUES (' + chimpad_pad_user + ',' + chimpad_pad_id + ',1;';
+            update_user_pads_query = 'INSERT INTO user_pad(user_id,pad_id,admin) VALUES (' + chimpad_pad_user + ',' + chimpad_pad_id + ',1);';
 
             client.query(update_user_pads_query , function(err, result) {
               done();
