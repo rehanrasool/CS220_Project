d29d908fb018f62463bedbbf075872f568b9d914
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-23 16:04:02 -0400

    test time since

--- public/js/pad.js
+++ public/js/pad.js
@@ -67,25 +67,28 @@ $(function(){
         var interval = Math.floor(seconds / 31536000);
 
         if (interval > 1) {
-            return interval + " years";
+            return interval + " years ago";
         }
         interval = Math.floor(seconds / 2592000);
         if (interval > 1) {
-            return interval + " months";
+            return interval + " months ago";
         }
         interval = Math.floor(seconds / 86400);
         if (interval > 1) {
-            return interval + " days";
+            return interval + " days ago";
         }
         interval = Math.floor(seconds / 3600);
         if (interval > 1) {
-            return interval + " hours";
+            return interval + " hours ago";
         }
         interval = Math.floor(seconds / 60);
         if (interval > 1) {
-            return interval + " minutes";
+            return interval + " minutes ago";
         }
-        return Math.floor(seconds) + " seconds";
+        if (Math.floor(seconds) < 5) {
+          return "just now";
+        }
+        return Math.floor(seconds) + " seconds ago";
     }
 
 });
