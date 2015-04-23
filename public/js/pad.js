9f92648197ce1792596d45fb8974c2ad50441743
Rehan Rasool <rehan.rasool9@gmail.com>
2015-04-23 16:01:10 -0400

    test time since

--- public/js/pad.js
+++ public/js/pad.js
@@ -17,7 +17,7 @@ $(function(){
       	var data = raw_data[0];
         $('#pad_title').html(data['title']);
         $('#pad_content').val(data['content']);
-        $('#pad_content_help_text').html('last modified: ' + data['last_modified_timestamp']);
+        $('#pad_content_help_text').html('last modified: ' + timeSince(new Date(data['last_modified_timestamp'])));
 
   	});
 
@@ -60,5 +60,32 @@ $(function(){
 
     }); 
 
+    function timeSince(date) {
+
+        var seconds = Math.floor((new Date() - date) / 1000);
+
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+    }
 
 });
