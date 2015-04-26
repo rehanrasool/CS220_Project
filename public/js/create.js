877b26c40864a90a2cb64e0cd996b24604223909
emOmar Waheed/em <emget_omar@outlook.com/em>
2015-04-25 21:00:27 -0400

    added create.js

--- public/js/create.js
+++ public/js/create.js
@@ -6,7 +6,7 @@ $(function(){
 	$('#add_collaborator_button').click(function(){
 		var collaborator_name = $('#inputCollaborators').val();
 		collaborator_array.push(collaborator_name);
-		var current_names = $('#collaborators_name').val();
+		var current_names = $('#collaborators_name').html();
 		$('#collaborators_name').html(current_names + "," + collaborator_name);
 		$('#inputCollaborators').val(""); //empty the field
 	});
