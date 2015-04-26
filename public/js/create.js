12a04225c312ffd4e9d9c2cc0b864545c2dbf733
emOmar Waheed/em <emget_omar@outlook.com/em>
2015-04-26 19:46:40 -0400

    adding search function in collaborators

--- public/js/create.js
+++ public/js/create.js
@@ -13,25 +13,31 @@ $(function(){
 		}
 	});
 
-
-$("#create_button").click(function(){
-	var chimpad_pad_title = $('#pad_title').val();
-        $.ajax
-          ({
-            type: "POST",
-            //the url where you want to sent the userName and password to
-            url: "/create_pad",
-            //json object to sent to the authentication url
-            data : {
-            pad_title : chimpad_pad_title,
-            pad_collaborators : collaborator_array
-          } }).done(function(raw_data) {
-              var pad_id = raw_data[0]['id'];
-              var url = "/pad/" + pad_id;    
-			  $(location).attr('href',url);          
-          });
-
+	$("#collaborator_search").bind('keyup', function(){
+       var text = $('#pad_content').val();
+        
     }); 
+
+
+
+	$("#create_button").click(function(){
+		var chimpad_pad_title = $('#pad_title').val();
+	        $.ajax
+	          ({
+	            type: "POST",
+	            //the url where you want to sent the userName and password to
+	            url: "/create_pad",
+	            //json object to sent to the authentication url
+	            data : {
+	            pad_title : chimpad_pad_title,
+	            pad_collaborators : collaborator_array
+	          } }).done(function(raw_data) {
+	              var pad_id = raw_data[0]['id'];
+	              var url = "/pad/" + pad_id;    
+				  $(location).attr('href',url);          
+	          });
+
+	    }); 
 
 
 
