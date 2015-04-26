37bfd70ecd590237341cb5e1e76251bfc0dd9630
emOmar Waheed/em <emget_omar@outlook.com/em>
2015-04-25 23:33:58 -0400

    create restructured

--- server_scripts.js
+++ server_scripts.js
@@ -211,7 +211,7 @@ module.exports = function(app, io) {
       });
     });
             
-    response.send(chimpad_pad_id);
+    response.sendStatus(chimpad_pad_id);
   });
 
 /*  function add_user_pads(username, pad_id){
