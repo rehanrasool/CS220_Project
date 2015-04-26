b25d43bd7a26fe78adadfd2e7230f42b54524aa0
emOmar Waheed/em <emget_omar@outlook.com/em>
2015-04-25 23:01:09 -0400

    create restructured

--- server_scripts.js
+++ server_scripts.js
@@ -171,7 +171,7 @@ module.exports = function(app, io) {
                       var collaborator_id = collaborators_array[collaborator];
                       add_user_pads(collaborator_id,chimpad_pad_id);
                     }
-                    response.redirect('/pad/' + chimpad_pad_id);
+                    response.render('/pad/' + chimpad_pad_id);
                   }
             });
             
@@ -203,7 +203,7 @@ module.exports = function(app, io) {
                       { console.error(err); response.send("Error " + err); }
                       else
                       { // dummy message
-                        
+
                       }
                 });
 
