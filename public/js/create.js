94fb65dde00eba771c1de92fb3cbb300e40baa83
emOmar Waheed/em <emget_omar@outlook.com/em>
2015-04-28 16:37:50 -0400

    fixed issue with add collaborators

--- public/js/create.js
+++ public/js/create.js
@@ -4,7 +4,7 @@ $(function(){
 	var collaborator_array = new Array();
 
 	$('#add_collaborator_button').click(function(){
-		var collaborator_name = $('#inputCollaborators').val();
+		var collaborator_name = $('#collaborators_list').val();
 		if(collaborator_name != ""){
 			collaborator_array.push(collaborator_name);
 			var current_names = $('#collaborators_name').html();
