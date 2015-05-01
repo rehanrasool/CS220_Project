7ebe67cbe547d4400a92e6a6ca4f7135c7a9fbec
Ronak Shah <ron94.s@gmail.com>
2015-05-01 19:59:57 -0400

    updated user profile

--- public/js/user.js
+++ public/js/user.js
@@ -24,20 +24,21 @@ $(function(){
 		}
 	}).done(function(raw_data) {
       
-      	var tbody = $('#user_pads_table tbody'),
-        	props = ["id", "title"];
+			var tbody = $('#user_pad_table tbody'),
+        	props = ["id", "title", "last_modified_timestamp", "username"];
     		$.each(raw_data, function(i, pad) {
     		  var tr = $('<tr>');
     		  $.each(props, function(i, prop) {
-            	if (prop == 'title') {
-             	 $('<td>').html('<a href="..\\pad\\' + pad['id'] + '">' + pad[prop] + '</a>').appendTo(tr);  
-            	} 
-            	else{
-              		$('<td>').html(pad[prop]).appendTo(tr);  
-            	}
+            if (prop == 'last_modified_timestamp') {
+              $('<td>').html(moment(new Date(pad[prop])).fromNow()).appendTo(tr);
+            } else if (prop == 'title') {
+              $('<td>').html('<a href="..\\pad\\' + pad['id'] + '">' + pad[prop] + '</a>').appendTo(tr);  
+            } else if(prop =='username'){
+              $('<td>').html('<a href="..\\user\\' + pad['last_modified_user'] + '">' + pad[prop] + '</a>').appendTo(tr);  
+            }else{
+              $('<td>').html(pad[prop]).appendTo(tr);  
+            }
     		  });
-    		  tbody.append(tr);
-    		});
-
+    		  tbody.append(tr);	
   	});
 });
