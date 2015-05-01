ef5c92269adb8c5ff94477a7d0720f089665abd2
Rehan Rasool <rehan.rasool9@gmail.com>
2015-05-01 19:40:20 -0400

    create about page

--- public/js/about.js
+++ public/js/about.js
@@ -8,9 +8,10 @@ $(function(){
       //json object to sent to the authentication url
       data : {
     } }).done(function(raw_data) {
-      $('#about_users_count').html(raw_data['users_count']);
-      $('#about_public_pads_count').html(raw_data['public_pads_count']);
-      $('#about_private_pads_count').html(raw_data['private_pads_count']);
+      var data = raw_data[0];
+      $('#about_users_count').html(data['users_count']);
+      $('#about_public_pads_count').html(data['public_pads_count']);
+      $('#about_private_pads_count').html(data['private_pads_count']);
 
   	});
 
