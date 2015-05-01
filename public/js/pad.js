a11289ed1bcfaa04264077d3821db65af7d43b93
Rehan Rasool <rehan.rasool9@gmail.com>
2015-05-01 15:06:15 -0400

    adding select for languages

--- public/js/pad.js
+++ public/js/pad.js
@@ -40,10 +40,10 @@ $(function(){
         $('#pad_content_last_modified_timestamp').html('last modified: ' + moment(new Date(data['last_modified_timestamp'])).fromNow());
         $('#pad_content_last_modified_user').html('last modified by : <a href="">' + data['last_modified_user'] + '</a>');
 
-        $("#pad_language_options").val(data['lang']);
+        /*$("#pad_language_options").val(data['lang']);
         $('pre code').each(function(i, block) {
           hljs.highlightBlock(block);
-        });
+        });*/
 
     });
 
