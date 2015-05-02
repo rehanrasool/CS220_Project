bde4b8eacb7ce0b8caf95efe345e4ea959dce89b
emOmar Waheed/em <emget_omar@outlook.com/em>
2015-05-01 20:54:46 -0400

    adding SQL injection

--- server_scripts.js
+++ server_scripts.js
@@ -186,7 +186,7 @@ module.exports = function(app, io) {
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
        
 
-        client.query('SELECT username FROM user_table WHERE username like $1%', [potential_name] , function(err, result) {
+        client.query('SELECT username FROM user_table WHERE username like \'$1%\'', [potential_name] , function(err, result) {
           done();
           if (err)
            { console.error(err); response.send("Error " + err); }
