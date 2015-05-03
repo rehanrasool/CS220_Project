4541605565986fc728d61a61d5e21b0cda9307a7
Rehan Rasool <rehan.rasool9@gmail.com>
2015-05-03 15:17:23 -0400

    post presentation cleanup

--- server_scripts.js
+++ server_scripts.js
@@ -1,6 +1,5 @@
 var pg = require('pg');
 var express = require('express');
-//var session = require('express-session');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')();
 var session = require('cookie-session')({secret: 'ssshhhhh'});
@@ -8,11 +7,9 @@ var session = require('cookie-session')({secret: 'ssshhhhh'});
 
 module.exports = function(app, io) {
 
-  //var sess;
-
   app.use(cookieParser);
   app.use(session);
-  //app.use(session);
+
   io.use(function(socket, next) {
     var req = socket.handshake;
     var res = {};
@@ -23,6 +20,7 @@ module.exports = function(app, io) {
   });
 
   app.use(express.static(__dirname + '/public'));
+  
     // parse application/json
   app.use(bodyParser.json());                        
   // parse application/x-www-form-urlencoded
@@ -46,16 +44,14 @@ module.exports = function(app, io) {
     });
   })
 
-
   app.post('/login', function(request, response) {
       var username = request.body.inputUsername;
       var password = request.body.inputPassword;
       console.log("post received: %s %s", username, password);
 
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        authenticate_query = 'SELECT id FROM user_table WHERE username = \'' + username + '\' AND password = \'' + password + '\';';
-        console.log(authenticate_query);
-        client.query(authenticate_query , function(err, result) {
+
+        client.query('SELECT id FROM user_table WHERE username = $1 AND password = $2;' , [username, password], function(err, result) {
           done();
           if (err)
            { console.error(err); response.send("Error " + err); }
@@ -66,14 +62,12 @@ module.exports = function(app, io) {
             sess=request.session;
             sess.user_id=id;
             sess.user_name=username;
-            //global.user_id = id;
 
             response.redirect('/home/'+id);
            }
         });
       });
   });
-
 
   app.post('/signup', function(request, response) {
       var username = request.body.inputUsername;
@@ -85,7 +79,7 @@ module.exports = function(app, io) {
 
         signup_query = 'INSERT INTO user_table (username,password,email) VALUES ( \'' + username + '\',\'' + password + '\',\'' + email + '\') RETURNING id;';
         console.log(signup_query);
-        client.query(signup_query , function(err, result) {
+        client.query('INSERT INTO user_table (username,password,email) VALUES ( $1,$2,$3) RETURNING id;',[username, password, email] , function(err, result) {
           done();
           if (err)
            { console.error(err); response.send("Error " + err); }
@@ -95,7 +89,6 @@ module.exports = function(app, io) {
 
             sess=request.session;
             sess.user_id=id;
-            //global.user_id = id;
 
             response.redirect('/home/'+id);
            }
@@ -131,8 +124,6 @@ module.exports = function(app, io) {
                     response.send(result.rows); 
                  }
               });
-
-            //response.send();
            }
         });
       });
@@ -143,14 +134,10 @@ module.exports = function(app, io) {
     var sess = request.session;
     var chimpad_pad_user = sess.user_id;
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        get_all_pads_data = 'SELECT p.id , p.title , p.content , date_part(\'epoch\' , p.last_modified_timestamp)*1000 as last_modified_timestamp , p.last_modified_user, u.username FROM pad p INNER JOIN user_table u ON (u.id = p.last_modified_user) WHERE type = \'public\' AND p.id NOT IN ( SELECT pad_id from user_pad WHERE user_id = ' + chimpad_pad_user + ') ORDER BY p.last_modified_timestamp DESC;';
 
         console.log(get_all_pads_data);
 
-/*        'SELECT id,title,content,date_part(\'epoch\',last_modified_timestamp)*1000 as last_modified_timestamp,last_modified_user FROM pad ORDER BY last_modified_timestamp DESC;';
-*/
-
-        client.query(get_all_pads_data , function(err, result) {
+        client.query('SELECT p.id , p.title , p.content , date_part(\'epoch\' , p.last_modified_timestamp)*1000 as last_modified_timestamp , p.last_modified_user, u.username FROM pad p INNER JOIN user_table u ON (u.id = p.last_modified_user) WHERE type = \'public\' AND p.id NOT IN ( SELECT pad_id from user_pad WHERE user_id = $1) ORDER BY p.last_modified_timestamp DESC;' , [chimpad_pad_user], function(err, result) {
           done();
           if (err)
            { console.error(err); response.send("Error " + err); }
@@ -168,9 +155,6 @@ module.exports = function(app, io) {
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         get_stats_query = 'SELECT (SELECT COUNT(id) FROM user_table) AS users_count,(SELECT COUNT(id) FROM pad WHERE type=\'public\') AS public_pads_count,(SELECT COUNT(id) FROM pad WHERE type=\'private\') AS private_pads_count';
 
-/*        'SELECT id,title,content,date_part(\'epoch\',last_modified_timestamp)*1000 as last_modified_timestamp,last_modified_user FROM pad ORDER BY last_modified_timestamp DESC;';
-*/
-
         client.query(get_stats_query , function(err, result) {
           done();
           if (err)
@@ -187,9 +171,7 @@ module.exports = function(app, io) {
   app.post('/get_user_profile',function(request,response){
     pg.connect(process.env.DATABASE_URL,function(err,client,done){
       var id=request.body.user_id;
-      /*
-      get_all_user_profile_data="SELECT id,username,email FROM user_table WHERE id = $1 ;";
-      */
+
       client.query("SELECT id,username,email FROM user_table WHERE id = $1 ;",[id],function(err,result){
         done();
         if(err)
@@ -207,9 +189,7 @@ module.exports = function(app, io) {
   app.post('/get_user_contributions',function(request,response){
     pg.connect(process.env.DATABASE_URL,function(err,client,done){
       var id=request.body.user_id;
-      /*
-      get_all_user_profile_data="SELECT id,username,email FROM user_table WHERE id = $1 ;";
-      */
+
       client.query("SELECT COUNT(pad_id) as number FROM user_pad WHERE user_id= $1 ;",[id],function(err,result){
         done();
         if(err)
@@ -227,9 +207,7 @@ module.exports = function(app, io) {
     app.post('/get_user_pad_ownerships',function(request,response){
     pg.connect(process.env.DATABASE_URL,function(err,client,done){
       var id=request.body.user_id;
-      /*
-      get_all_user_profile_data="SELECT id,username,email FROM user_table WHERE id = $1 ;";
-      */
+
       client.query("SELECT COUNT(pad_id) as number FROM user_pad WHERE user_id= $1 AND admin=1;",[id],function(err,result){
         done();
         if(err)
@@ -247,9 +225,7 @@ module.exports = function(app, io) {
     app.post('/get_user_skills',function(request,response){
     pg.connect(process.env.DATABASE_URL,function(err,client,done){
       var id=request.body.user_id;
-      /*
-      get_all_user_profile_data="SELECT id,username,email FROM user_table WHERE id = $1 ;";
-      */
+
       client.query("SELECT DISTINCT lang FROM pad WHERE id IN (SELECT pad_id FROM user_pad WHERE user_id= $1) ;",[id],function(err,result){
         done();
         if(err)
@@ -267,9 +243,8 @@ module.exports = function(app, io) {
     app.post('/get_user_public_pads',function(request,response){
     pg.connect(process.env.DATABASE_URL,function(err,client,done){
       var chimpad_user_id=request.body.user_id;
-      get_user_pads_id_list = 'SELECT pad_id FROM user_pad WHERE user_id = \'' + chimpad_user_id + '\';';
 
-        client.query(get_user_pads_id_list , function(err, result) {
+        client.query('SELECT pad_id FROM user_pad WHERE user_id = $1;' ,[chimpad_user_id] function(err, result) {
           done();
           if (err)
            { console.error(err); response.send("Error " + err); }
@@ -325,9 +300,6 @@ module.exports = function(app, io) {
     var chimpad_pad_id = 1;
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      /*save_or_update_pad_query = 'INSERT INTO pad (title,last_modified_timestamp,last_modified_user,type) VALUES (\'' + chimpad_pad_title + '\', NOW() ,' + chimpad_pad_user + ',\'' + chimpad_pad_type +'\' ) RETURNING id;';
-      console.log(save_or_update_pad_query);*/
-
       
       client.query('INSERT INTO pad (title,last_modified_timestamp,last_modified_user,type) VALUES ($1,NOW(),$2,$3) RETURNING id;',[chimpad_pad_title,chimpad_pad_user,chimpad_pad_type] , function(err, result) {
         done();
@@ -391,38 +363,6 @@ module.exports = function(app, io) {
             
   });
 
-/*  function add_user_pads(username, pad_id){
-
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            var get_username_from_id_query = 'SELECT id from user_table WHERE username = \'' + username + '\'' + ';';
-
-            client.query(get_username_from_id_query, function(err,result){
-              done();
-              if (err){
-                console.error(err);
-                response.send("Error " + err);
-              }else{
-                pad_user_id = result.rows[0]['id'];
-
-                update_user_pads_query = 'INSERT INTO user_pad(user_id,pad_id,admin) VALUES (' + pad_user_id + ',' + pad_id + ',0);'; // not an admin
-
-
-                client.query(update_user_pads_query , function(err, result) {
-                      done();
-                      if (err)
-                      { console.error(err); response.send("Error " + err); }
-                      else
-                      { // dummy message
-
-                      }
-                });
-
-              }
-            });
-
-         });
-  }*/
-
   //save content on pressing the save button
   app.post('/save_pad', function(request, response) {
     sess=request.session;
@@ -432,9 +372,6 @@ module.exports = function(app, io) {
     var chimpad_pad_user = sess.user_id;
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      //save_or_update_pad_query = 'UPDATE pad SET last_modified_timestamp = NOW() ,content =  \'' + chimpad_pad_content + '\' ,last_modified_user = '+ chimpad_pad_user + ' WHERE id = ' + chimpad_pad_id + ';';
-
-      //save_or_update_pad_query = 'UPDATE pad SET last_modified_timestamp = NOW() ,content =  $1 ,last_modified_user = $2 WHERE id = $3;', chimpad_pad_content, chimpad_pad_user, chimpad_pad_id;
 
       client.query('UPDATE pad SET last_modified_timestamp = NOW() ,content = $1 ,last_modified_user = $2, lang = $3 WHERE id = $4;', [chimpad_pad_content, chimpad_pad_user, chimpad_pad_language, chimpad_pad_id ], function(err, result) {
         done();
@@ -510,7 +447,6 @@ module.exports = function(app, io) {
       var chimpad_pad_id = request.body.pad_id;
 
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        //get_pad_query = 'SELECT id,title,content,date_part(\'epoch\',last_modified_timestamp)*1000 as last_modified_timestamp,last_modified_user,lang FROM pad WHERE id = ' + chimpad_pad_id + ';';
 
         get_pad_query = 'SELECT p.id , p.title , p.content , date_part(\'epoch\' , p.last_modified_timestamp)*1000 as last_modified_timestamp , p.last_modified_user, p.lang, u.username FROM pad p INNER JOIN user_table u ON (u.id = p.last_modified_user) WHERE p.id = ' + chimpad_pad_id + ';';
 
@@ -576,53 +512,6 @@ module.exports = function(app, io) {
       });
   });
 
-
-/**
- Deletes a pad and return true if deletion was successful.
- 
- If user is admin and the pad exists then it is deleted -> return true
- 
- If user is not an admin then he does not have the permission to delete the 
- pad and thus -> return false
-**/
-
-/*
-   app.post('/delete_pad', function(request, response) {
-        sess=request.session;
-        var chimpad_user_id = sess.user_id; // user's id
-        var chimpad_pad_id = request.body.pad_id; // pad's id
-
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        check_if_user_admin_query = 'SELECT admin FROM user_pad WHERE user_id = ' + chimpad_user_id + ' AND pad_id =' + chimpad_pad_id + ';';
-           client.query(check_if_user_admin_query , function(err, result) {
-            done();
-            if (err)
-             { console.error(err); response.send("Error " + err); }
-            else
-            { 
-              if(result.row[0].admin == 1){//user is admin, so remove all the users from this pad and delete pad -> return true
-                 remove_users_from_this_pad = 'DELETE FROM user_pad WHERE pad_id = '+ chimpad_pad_id + ';'; // delete all user's pad with this id
-                 client.query(check_if_user_admin_query , function(err, result) {
-                 done();
-                 if (err)
-                   { console.error(err); response.send("Error " + err); }
-                  else
-                  {// all pads from user's now deleted, now delete the pad itself
-                    delete_pad(chimpad_pad_id);  
-                    return true;            
-                  }
-              });
-              }else{// user not admin, so just return false
-                 return false;
-         
-              });
-            }
-          });
-        });
-    });
-
-*/
-    
 //gets all users
   app.post('/get_all_users', function(request, response) {
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
